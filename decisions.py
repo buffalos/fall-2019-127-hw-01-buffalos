48fb64a6082193e7cf794426928b98840d17057c
buffalos <bahtija.durakovic61@myhunter.cuny.edu>
2019-09-13 11:02:27 -0400

    helloname

---
+++ decisions.py
@@ -0,0 +1,16 @@
+name = "Jimmy B"
+
+if name =="James Bond":
+    print("Bond, James Bond")
+    
+if True:
+    print("This always runs")
+    
+if False:
+    print("This never runs")
+    
+if name == "James Bond" or name=="Jimmy B":
+    print("DO you expect me to talk?")
+    print("No Mr. Bond, but I expect you to die.")
+
+print("This is after the last if")
